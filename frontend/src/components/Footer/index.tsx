7977ce481198656d6ef37dd030423b3c8d957e8b
Efraim Tenorio <efratec20.tenorio@gmail.com>
2021-11-03 19:01:34 -0300

    DataTable

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () =>  {
+    return (<footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+            <p className="text-light">App developed by the <a href="https://github.com/efratec" target="_blank" rel="noreferrer">Efraim Tenorio</a></p>
+            <p className="text-light"><small><strong>Week Spring React</strong><br />
+                Event promoted by the DevSuperior School: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+        </div>
+    </footer>
+    );
+}
+  
+  export default Footer;
