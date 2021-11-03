63f1871c20f47eab8746d83d969f3b2ec104aa43
Efraim Tenorio <efratec20.tenorio@gmail.com>
2021-11-03 20:43:42 -0300

    Security-Config

---
+++ back-end/src/main/java/com/devsuperior/dsvendas/model/Seller.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dsvendas.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Seller implements Serializable {
+
+    @Id
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Seller seller = (Seller) o;
+        return Objects.equals(id, seller.id) && Objects.equals(name, seller.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+}
